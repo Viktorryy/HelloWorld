375039cc4aca451db422f6897920edf68ffeb4d7
Viktorryy <viktory_o@rambler.ru>
2022-09-13 17:55:40 +0400

    поменяла вывод еще раз 3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет большой мир!");
+        System.out.println("Привет мой большой мир!");
     }
 
 }
